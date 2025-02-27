59792fef61b1460b488af399fcdb2041c04639ab
Wendell dos Santos <wendell_dsantos@hotmail.com>
2025-02-27 17:13:26 -0300

    feat (input) - adiciona leitura de arquivos + demo do menu de opcoes

---
+++ io_utils.py
@@ -0,0 +1,21 @@
+example_trace = "../test_examples/trace_shannon_48"
+
+def display_options(prompt_text, options_list):
+    print(prompt_text)
+    options = [f'({i+1}) {option}' for i, option in enumerate(options_list)]
+    options_numbers = (i+1 for i in range(len(options)))
+    for formatted_option in options:
+        print(formatted_option)
+    return options_numbers
+    
+def read_file(file_path):
+    retry = True
+    while retry:
+        # file_path = input(f"Digite o caminho do arquivo {file_type} ou arraste para cá: ")
+        try:
+            with open(file_path, "r") as file:
+                lines = [line.split() for line in file.readlines()]
+                return lines
+        except FileNotFoundError:
+            input(f"Erro na leitura. Verifique o diretório.")
+        
