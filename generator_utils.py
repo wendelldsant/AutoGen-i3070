79753924717b55879d6aad438d051fcebd84b68c
Wendell dos Santos <wendell_dsantos@hotmail.com>
2025-03-05 13:48:39 -0300

    feat: adiciona funcao de geracao power nodes

--- generator_utils.py
+++ generator_utils.py
@@ -1,4 +1,5 @@
 import analysis_utils as analysis
+import io_utils as io
 
 def probe_size_separator(wires_path, inserts_path, probe_size):
     result = []
@@ -12,3 +13,32 @@ def probe_size_separator(wires_path, inserts_path, probe_size):
                 result.append(node_wires)
                 continue
     return result
+
+def power_nodes(powers_list_path, inserts_path):
+    result = []
+    powers_list = io.read_file(powers_list_path)
+    inserts = analysis.inserts_filtered(inserts_path)
+    tolerance = io.get_number("Digite a tolerância padrão: ")
+    for line in powers_list:
+        power = line[0]
+        test_line = f'!test "{power}"  !NODE_NOT_FOUND'
+        for node in inserts:
+            if node["Node_Name"] == power:
+                test_line = f'test "{power}"'
+                break
+        voltage = io.get_number(f"Digite o valor da tensão {power}: ")
+        subtest = f"""
+        subtest "{power}"
+            connect i to nodes "{power}"
+            connect l to nodes ground
+            detector dcv, expect {voltage}
+            measure  {voltage + tolerance}, {voltage - tolerance}
+        end subtest
+        """
+        test_info = (power, test_line, subtest)
+        result.append(test_info)
+    return result
+
+power_list = "./test_examples/nodes_list_pwr_nodes"
+inserts = "./test_examples/inserts_shannon_48"
+    
